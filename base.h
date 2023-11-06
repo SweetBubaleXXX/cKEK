59b22b80f6fb927f476987a7371fa21be89fb5aa
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-11-06 15:32:38 +0300

    Add base key classes

---
+++ base.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+
+namespace Base {
+    class Key
+    {
+    protected:
+        int key_size;
+    public:
+        virtual int get_key_size() = 0;
+        virtual void serialize(const char* password, std::ostream) = 0;
+    };
+
+    class PublicKey : virtual Key
+    {
+    public:
+        virtual void encrypt(const std::istream, std::ostream) = 0;
+        virtual void verify(const std::istream, std::ostream) = 0;
+    };
+
+    class PrivateKey : virtual Key
+    {
+    public:
+        virtual PublicKey gen_public_key() = 0;
+        virtual void decrypt(const std::istream, std::ostream) = 0;
+        virtual void sign(const std::istream, std::ostream) = 0;
+    };
+}
